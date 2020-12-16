da43c51721cf4b5c5c41add2137d9bd11fca45e9
Moritz Willmann <moritz.willmann@uni-ulm.de>
2020-12-16 06:39:26 +0100

    Update Day15 without array

--- Day15/memory.py
+++ Day15/memory.py
@@ -1,18 +1,17 @@
 def memory1(init_vals, i_desired):
-    vals = [0] * i_desired
-    vals[0:len(init_vals)] = init_vals
-
     last_seen = {}
     for i, val in enumerate(init_vals):
         last_seen[val] = i
     
+    prev = val
     for i in range(len(init_vals), i_desired):
-        if vals[i-1] in last_seen.keys():
-            vals[i] = i - last_seen[vals[i-1]] - 1
+        if prev in last_seen.keys():
+            curr = i - last_seen[prev] - 1
         else:
-            vals[i] = 0
-        last_seen[vals[i-1]] = i-1
-    return vals[-1]
+            curr = 0
+        last_seen[prev] = i-1
+        prev = curr
+    return curr
 
 input = [5, 2, 8, 16, 18, 0 , 1]
 print("Part 1: {}, Part 2: {}".format(memory1(input, 2020), memory1(input, 30000000)))
